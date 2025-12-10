db07590355c5db34222439237ee7a3ada91b6798
xxxmoney <xxxmoney111@gmail.com>
2025-12-10 16:53:53 +0100

    Cancellation, smearing

--- src/01_number_representation/cancellation.py
+++ src/01_number_representation/cancellation.py
@@ -1,1 +1,19 @@
-# TODO: implement
+#
+# Cancellation causes loosing the precision
+# It happens when we have very big numbers, close to each other - with subtraction the "small" part disappears due to precision issues
+#
+
+
+def subtract(number_one: float, number_two: float):
+    result = number_one - number_two
+
+    print(result)
+
+print("Can be represented correctly:")
+subtract(10.5, 10)
+
+print("Still represented correctly:")
+subtract(1000000.5, 1000000)
+
+print("Loosing precision:")
+subtract(1000000.0000000000005, 1000000)
--- src/01_number_representation/smearing.py
+++ src/01_number_representation/smearing.py
@@ -1,1 +1,17 @@
-# TODO: implement
+
+#
+# Smearing causes loosing the precision
+# It happens when we have one big and one very small numbers, and we are adding them - the bigger number is not affected as we expected
+#
+
+def add(number_one: float, number_two: float):
+    result = number_one + number_two
+
+    print(result)
+
+
+print("Does not cause issues:")
+add(100000, 100)
+
+print("Causes issues - we have big number and very small number:")
+add(100000, 0.0000000000005)
