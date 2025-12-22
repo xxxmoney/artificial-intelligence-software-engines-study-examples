cb9123f9d48219d93a86c1eb6f82f52f3ed5de37
xxxmoney <xxxmoney111@gmail.com>
2025-12-22 14:04:11 +0100

    WIP nodes for Behavior Tree

--- src/03_behavior/behavior_tree.py
+++ src/03_behavior/behavior_tree.py
@@ -5,11 +5,13 @@
 # - Selector - list of nodes - successful if any of the nodes returns SUCCESSFUL or RUNNING state (so it basically takes the first successful one)
 # - Sequence - list of nodes - all of them have to be successful (SUCCESSFUL/RUNNING)
 # Each leaf can be condition or action
-# - Condition - simply returns TRUE or FALSE (like checking if has enough health, etc)
+# - Condition - simply returns SUCCESS or FAILURE (like checking if has enough health, etc)
 # - Action - takes action - attacks enemy, heals, etc
 #
 
 from enum import Enum
+from typing import Callable
+
 from game_simple import GameSimple
 
 class Status(Enum):
@@ -17,3 +19,58 @@ class Status(Enum):
     FAILURE = 1,
     RUNNING = 2
 
+class Node:
+    def tick(self, game: GameSimple) -> Status:
+        raise NotImplementedError("Method tick not implemented")
+
+class Selector(Node):
+    children: list[Node]
+
+    def __init__(self, children: list[Node]):
+        self.children = []
+#
+    def tick(self, game: GameSimple):
+        # Checks for the first child that is not FAILURE
+        for child in self.children:
+            status = child.tick(game)
+            if status != Status.FAILURE:
+                return status
+
+        return Status.FAILURE
+
+class Sequence(Node):
+    children: list[Node]
+
+    def __init__(self, children: list[Node]):
+        self.children = children
+
+    def tick(self, game: GameSimple):
+        # Stops checking if any child is not SUCCESS
+        for child in self.children:
+            status = child.tick(game)
+            if status != Status.SUCCESS:
+                return status
+
+        return Status.SUCCESS
+
+class Condition(Node):
+    predicate_fn: Callable[[GameSimple], bool]
+
+    def __init__(self, predicate_fn):
+        self.predicate_fn = predicate_fn
+
+    def tick(self, game: GameSimple):
+        return Status.SUCCESS if self.predicate_fn(game) else Status.FAILURE
+
+class Action(Node):
+    action_fn: Callable[[GameSimple], None]
+
+    def __init__(self, action_fn):
+        self.action_fn = action_fn
+
+    def tick(self, game: GameSimple):
+        try:
+            self.action_fn(game)
+            return Status.SUCCESS
+        except:
+            return Status.FAILURE
