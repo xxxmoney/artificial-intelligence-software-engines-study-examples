3c5fe688bbb666e19c76cddfd288982d9929e509
xxxmoney <xxxmoney111@gmail.com>
2025-12-31 14:17:56 +0100

    WIP wave function collapse

--- src/05_procedural_generation/wave_function_collapse.py
+++ src/05_procedural_generation/wave_function_collapse.py
@@ -1,3 +1,8 @@
+import random
+from enum import Enum
+from typing import Optional
+
+
 #
 # WFC - Wave Function Collapse
 # It's a way to procedurally generate maps for games based on rules
@@ -11,5 +16,175 @@
 # - And this now cascades further - and this repeats until the whole grid is defined
 #
 
+class TileType(Enum):
+    MOUNTAIN = 1
+    GRASS = 2
+    LAKE = 3
+    CASTLE = 4
+
+    @staticmethod
+    def get_all() -> set['TileType']:
+        return set(list(TileType))
+
+    @staticmethod
+    def get_rules() -> dict['TileType', set['TileType']]:
+        return {
+            TileType.MOUNTAIN: {
+                TileType.MOUNTAIN,
+                TileType.GRASS,
+                TileType.LAKE
+            },
+            TileType.GRASS: {
+                TileType.GRASS,
+                TileType.LAKE
+            },
+            TileType.LAKE: {
+                TileType.GRASS
+            },
+            TileType.CASTLE: {
+                TileType.MOUNTAIN,
+                TileType.LAKE
+            }
+        }
+
+    @staticmethod
+    def get_possible_types(options: set['TileType']) -> set['TileType']:
+        compatible = TileType.get_all()
+        rules = TileType.get_rules()
+
+        for n in options:
+            compatible &= rules[n]
+
+        return compatible
+
+    def __str__(self):
+        match self:
+            case TileType.MOUNTAIN:
+                return "M"
+            case TileType.GRASS:
+                return "G"
+            case TileType.LAKE:
+                return "L"
+            case TileType.CASTLE:
+                return "C"
+            case _:
+                return "_"
+
+class Tile:
+    _type: Optional[TileType] # The type of this tile
+    _possible_types: set[TileType] # Represents which types this can turn into - if not None, has only one - the type
+
+    def __init__(self, type: Optional[TileType]):
+        self._type = type
+        self._possible_types = [type] if type is not None else TileType.get_all()
+
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, type: TileType):
+        if self._type is not None:
+            raise Exception('Cannot set type twice')
+
+        self._type = type
+        self._possible_types = {type}
+
+    @property
+    def possible_types(self):
+        return self._possible_types
+
+    def reduce_possible_types(self, reduced_types: set[TileType]):
+        if self._type is not None:
+            raise Exception('Cannot reduce types for defined type')
+
+        self._possible_types -= reduced_types
+
+    def __str__(self):
+        return f"[{self.type}: {self.possible_types}]"
+
+class Grid:
+    row_count: int
+    column_count: int
+    _tiles: list[list[Tile]] # First list - rows - - each list for each row is columns
+
+    def __init__(self, row_count: int, column_count: int):
+        self.row_count = row_count
+        self.column_count = column_count
+
+        # Set default grid by row count and column count
+        self._tiles = [[Tile(None) for _ in range(row_count)] for _ in range(column_count)]
+
+    def get_tile_type(self, row: int, column: int) -> TileType:
+        return self._tiles[row][column].type
+
+    def set_tile_type(self, row: int, column: int, type: TileType):
+        self._tiles[row][column].type = type
+
+    def has_tile(self, row: int, column: int) -> bool:
+        return self.get_tile_type(row, column) is not None
+
+    def has_any_tile(self) -> bool:
+        for row in self._tiles:
+            for tile in row:
+                if tile is not None:
+                    return True
+
+        return False
+
+    def reduce_possible_tile_types(self, row: int, column: int):
+        tile = self._tiles[row][column]
+        possible_types = TileType.get_possible_types(tile.possible_types)
+
+        for row, column in self.get_neighbour_positions(row, column):
+            neighbour = self._tiles[row][column]
+            # Neighbour has possible types - by reducing the set possible types for neighbours we get those that the neighbour cannot be - reduced types
+            reduced_types = neighbour.possible_types - possible_types
+
+            # Only reduce and apply to neighbour if can reduce anything
+            if neighbour.type is None and reduced_types:
+                neighbour.reduce_possible_types(reduced_types)
+                self.reduce_possible_tile_types(row, column)
+
+    def get_neighbour_positions(self, row: int, column: int) ->  list[tuple[int, int]]:
+        positions: list[tuple[int, int]] = []
+
+        # Left
+        if row > 0:
+            positions.append((row - 1, column))
+        # Right
+        if row < self.row_count - 1:
+            positions.append((row + 1, column))
+        # Up
+        if column > 0:
+            positions.append((row, column - 1))
+        # Down
+        if column < self.column_count - 1:
+            positions.append((row, column + 1))
+
+        return positions
+
+    def __str__(self):
+        rows = []
+
+        for row in self._tiles:
+            rows.append("\t".join([str(tile) for tile in row]))
+
+        return "\n".join(rows)
+
+class WaveFunctionCollapse:
+    grid: Grid
+
+    def __init__(self, grid: Grid):
+        self.grid = grid
 
 
+if __name__ == "__main__":
+    grid = Grid(5, 5)
+
+    grid.set_tile_type(0, 0, TileType.MOUNTAIN)
+    grid.reduce_possible_tile_types(0, 0)
+
+    #wfc = WaveFunctionCollapse(grid)
+
+    print(grid)
